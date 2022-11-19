8d63181207539e4f9699fda95a15c159f36b3170
Murtada Altarouti <murtada.altarouti@gmail.com>
2022-11-19 22:07:54 +0300

    Updated observer pattern

--- src/behavioral/observer/main.py
+++ src/behavioral/observer/main.py
@@ -9,7 +9,11 @@ if __name__ == "__main__":
     observer_a = ConcreteObserverA()
     observer_b = ConcreteObserverB()
 
+    # State 1:
     subject.attach(observer_a)
     subject.attach(observer_b)
+    subject.notify()
 
+    # State 2
+    subject.deatach(observer_b)
     subject.notify()
